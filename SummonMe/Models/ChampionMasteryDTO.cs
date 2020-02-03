5edc434e828d1c8a6a633d19e71ba5efb0c7d3ee
kutluToren <kutlutoren@gmail.com>
2020-02-03 20:08:55 +0100

    modified classes for api

---
+++ SummonMe/Models/ChampionMasteryDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummonMe.Models
+{
+    public class ChampionMasteryDTO
+    {
+        public bool ChestGranted { get; set; }
+        public int ChampionLevel { get; set; }
+        public int ChampionPoints { get; set; }
+        public long ChampionId { get; set; }
+        public long ChampionPointsUntilNextLevel { get; set; }
+        public long LastPlayTime { get; set; }
+        public int TokensEarned { get; set; }
+        public long ChampionPointsSinceLastLevel { get; set; }
+        public string SummonerId { get; set; }
+
+}
+}
--- SummonMe/Models/LeagueEntryDTO.cs
+++ SummonMe/Models/LeagueEntryDTO.cs
@@ -13,5 +13,12 @@ namespace SummonMe.Models
         public int Wins { get; set; }
         public int Losses { get; set; }
         public string Tier { get; set; }
+        public bool HotStreak { get; set; }
+        public bool Veteran { get; set; }
+        public string Rank { get; set; }
+        public int LeaguePoints { get; set; }
+        public string SummonerId { get; set; }
+        public bool Inactive { get; set; }
+        public bool FreshBlood { get; set; }
     }
 }
