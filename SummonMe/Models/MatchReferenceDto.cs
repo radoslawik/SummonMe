26395ede27fe3e2e143fb5116e6b1a2d9a8866dd
kutluToren <kutlutoren@gmail.com>
2020-02-03 20:30:17 +0100

    adding classes for api requests

---
+++ SummonMe/Models/MatchReferenceDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummonMe.Models
+{
+    public class MatchReferenceDto
+    {
+        public string Lane { get; set;}
+        public long GameId { get; set;}
+        public int Champion { get; set;}
+        public string PlatformID { get; set;}
+        public int Season { get; set; }
+        public int Queue { get; set; }
+        public string Role { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
---
+++ SummonMe/Models/MatchlistDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummonMe.Models
+{
+    class MatchlistDto
+    {
+        public List<MatchReferenceDto> Matches { get; set; }
+        public int TotalGames { get; set; }
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
+    }
+}
